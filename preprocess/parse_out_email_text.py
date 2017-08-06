2c211a898384add75d9bea0cd3c6372fca8b41e5
linhung0319 <linhung0319@gmail.com>
2017-08-06 09:21:02 +0800

    Load email content and remove punctuation
    - load email and split the metadata into email content
    - remove punctuations in the content
    - replace \n,\t,\r with white space

--- preprocess/parse_out_email_text.py
+++ preprocess/parse_out_email_text.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
-from nltk.stem.snowball import SnowballStemmer
+import logging
+logging.basicConfig(level=logging.DEBUG,
+		    format='%(name)s - %(message)s')
 
-def parseOutText(f)
+from nltk.stem.snowball import SnowballStemmer
+import string
+
+def parseOutText(f):
 	"""
 	   given an opened email file f, parse out all text
 	   below the metadata block at the top 
@@ -15,10 +20,28 @@ def parseOutText(f)
 	   text = parseOutText(f)
 	"""
 
-def main()
-	f = open("test_email.txt", "r")
-	text = parseOutText(f)
-	print text
+	logger = logging.getLogger(parseOutText.__name__)
+	### go back to the beginning of the file and read all the text
+	f.seek(0)
+	all_text = f.read()
+	logger.debug('all_text:\n%s', all_text)
+	### split off metadata
+	content = all_text.split("X-FileName:")
+	### if email has content
+	if len(content) > 1:
+		logger.debug('content:\n%s', content[1])
+		### remove punctuation
+		text_string = content[1].translate(str.maketrans("", "", string.punctuation))
+		### replace \n, \t, \r with white space
+		for i in ['\n', '\t', '\r']:
+			text_string = text_string.replace(i, " ")
+		logger.debug('content(remove punctuation):\n%s', text_string) 
+		###
 
-if __name__ == 'main':
+def main():
+	f = open("test_email.txt", "r")
+	parseOutText(f)
+
+
+if __name__ == '__main__':
 	main()

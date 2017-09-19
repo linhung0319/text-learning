eca6c68ffe6e5f906a84de7f1732dc3e0d9d44dd
linhung0319 <linhung0319@gmail.com>
2017-09-19 20:12:58 +0800

    Change function name (find_best param)
    - (test_different_kernel_in_svm) -> (find_best_param)
    - return a dict object {'C', 'gamma', 'kernel'}
    - can find the best parameter in a specific kernel

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -16,6 +16,8 @@ from sklearn.metrics import make_scorer, accuracy_score
 from email_preprocess import preprocess
 from plot_gallery import plot_accuracy_score
 
+import matplotlib.pyplot as plt
+
 def main():
 	""" 
 	    Use a SVM to identify emails from the Enron corpus by their authors:    
@@ -32,19 +34,31 @@ def main():
 	scaled_features = features_train[:int(len(features_train) / 10)]
 	scaled_labels = labels_train[:int(len(labels_train) / 10)]
 
-	test_different_kernel_in_svm(scaled_features, scaled_labels)
+	### find the best parameter (kernel, C, gamma) in SVM
+	param = find_best_param(scaled_features, scaled_labels,
+				scoring=make_scorer(accuracy_score),
+				C_range=np.logspace(-2, 10, 13),
+				gamma_range=np.logspace(-9, 3, 13),
+				kernel_range=('linear', 'rbf'),
+				kernel='linear')
+	###
+	
 
-def test_different_kernel_in_svm(features, labels):
+def find_best_param(features, labels, scoring, C_range, gamma_range, kernel_range, kernel=None):
 	"""
 	    scoring - the method to determine the accuracy in SVM
 	    C_range, gamma_range - tuning parameters in SVM
 	    kernel_range - different kernel in SVM, ex: 'linear', 'rbf', 'poly'
-	"""	
-	scoring = make_scorer(accuracy_score)
-	C_range = np.logspace(-2, 10, 13)
-	gamma_range = np.logspace(-9, 3, 13)
-	kernel_range = ('linear', 'rbf')
 
+	    kernel - choose the best parameter in a given kernel
+	    
+	    if kerel = None, this function will return the best parameter across all kernel_range
+
+	    1 dict object is returned:
+	    	-- {'C', 'gamma', 'kernel'}
+	"""
+	logger = logging.getLogger(find_best_param.__name__)
+	
 	param_grid = {'C': C_range,
 		      'gamma': gamma_range,
 		      'kernel': kernel_range}
@@ -54,7 +68,18 @@ def test_different_kernel_in_svm(features, labels):
 	### score['C']['gamma']['kernel']
 	score = clf.cv_results_['mean_test_score'].reshape(len(C_range), len(gamma_range), len(kernel_range))
 	
+	### plot the accuracy score 
 	plot_accuracy_score(score, C_range, gamma_range, kernel_range)
+	
+	if kernel == None:
+		return clf.best_params_
+	elif kernel not in kernel_range:
+		logger.info('Your chosen kernel is not in kernel range, return the best parameter')
+		return clf.best_params_
+	else:
+		kernel_index = kernel_range.index(kernel)	
+		index = np.unravel_index(score[:, :, kernel_index].argmax(), score[:, :, kernel_index].shape)
+		return {'C': C_range[index[0]], 'gamma': gamma_range[index[1]], 'kernel': kernel}
 
 if __name__ == '__main__':
 	main()
